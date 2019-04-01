cfced60de507f31b0ed5f81f1fe9032d4981af0a
anjiho <anjo0080@gmail.com>
2019-04-01 16:09:35 +0900

    초기 소스 구성

---
+++ src/main/java/com/zianedu/lms/utils/CreateSESSION.java
@@ -0,0 +1,15 @@
+package com.zianedu.lms.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class CreateSESSION {
+
+	final static Logger logger = LoggerFactory.getLogger(CreateSESSION.class);
+
+}
