4712d9db24ab2c06de48ff1c0395a5e7c885fe62
anjiho <anjo0080@gmail.com>
2019-04-19 15:31:49 +0900

    ./

--- src/main/java/com/zianedu/lms/mapper/ProductManageMapper.java
+++ src/main/java/com/zianedu/lms/mapper/ProductManageMapper.java
@@ -132,7 +132,7 @@ public interface ProductManageMapper {
 
     void deleteTBankSubjectQuesLink(@Param("bankSubjectQuesLinkKey") int bankSubjectQuesLinkKey);
 
-    void deleteTGoodsPriceOpition(@Param("gKey") int gKey);
+    void deleteTGoodsPriceOption(@Param("gKey") int gKey);
 
     /** UPDATE **/
     void updateTGoods(TGoodsVO tGoodsVO);
