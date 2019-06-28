16d002b74b5ddd6b8e40ca5073d3382712c019a2
anjiho <anjo0080@gmail.com>
2019-06-28 15:06:07 +0900

    ./

--- src/main/java/com/zianedu/lms/service/MemberManageService.java
+++ src/main/java/com/zianedu/lms/service/MemberManageService.java
@@ -344,8 +344,8 @@ public class MemberManageService {
         String tableName = "SC_LOG_" + yyyyMM;
 
         SmsSearchParamDTO paramDTO = new SmsSearchParamDTO(
-                startNumber, listLimit, Util.isNullValue(tableName, ""), Util.isNullValue(searchText, ""), Util.isNullValue(searchType, "")
-        );
+                startNumber, listLimit, Util.isNullValue(tableName, ""), Util.isNullValue(searchType, ""), Util.isNullValue(searchText, "")
+                );
         List<SmsSendListDTO>list = new ArrayList<>();
         try {
             list = memberManageMapper.selectSmsSendLogList(paramDTO);
@@ -374,7 +374,7 @@ public class MemberManageService {
         int cnt = 0;
         String tableName = "SC_LOG_" + yyyyMM;
         SmsSearchParamDTO paramDTO = new SmsSearchParamDTO(
-                Util.isNullValue(tableName, ""), Util.isNullValue(searchText, ""), Util.isNullValue(searchType, "")
+                Util.isNullValue(tableName, ""), Util.isNullValue(searchType, ""), Util.isNullValue(searchText, "")
         );
         try {
             cnt = memberManageMapper.selectSmsSendLogListCount(paramDTO);
