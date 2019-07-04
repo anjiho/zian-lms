237a70e972a92f06599bd841d5d744d3c7273252
anjiho <anjo0080@gmail.com>
2019-07-04 13:58:02 +0900

    ./

--- src/main/java/com/zianedu/lms/mapper/OrderManageMapper.java
+++ src/main/java/com/zianedu/lms/mapper/OrderManageMapper.java
@@ -143,6 +143,8 @@ public interface OrderManageMapper {
 
     void updateGoodsKindType(@Param("jId") String jId, @Param("kind") int kind);
 
+    void updateOrderLec(TOrderLecVO tOrderLecVO);
+
     /** DELETE **/
     void deleteTDeviceLimit(@Param("deviceLimitKey") int deviceLimitKey);
 }
