695d522e645d20ecb70a8e584a68c428646164bd
woneunjoeng <huuc10@gmail.com>
2019-07-04 17:25:34 +0900

    ./

--- src/main/webapp/common/js/selectbox.js
+++ src/main/webapp/common/js/selectbox.js
@@ -620,8 +620,6 @@ function getTeacherSubjectCategoryList4(val) {
             }
         }
         html += "</select>";
-        //innerHTML(tagId, html);
-        //$("#prod_list").find("tbody").find("tr").eq(index).find("td").eq(0).html(html);
         $(".testContent").find("span").eq(0).html(html);
     });
 }
@@ -639,8 +637,6 @@ function getTeacherSubjectCategoryList9(val) {
             }
         }
         html += "</select>";
-        //innerHTML(tagId, html);
-        //$("#prod_list").find("tbody").find("tr").eq(index).find("td").eq(0).html(html);
         $(".testContent1").find("span").eq(0).html(html);
     });
 }
@@ -657,8 +653,6 @@ function getTeacherSubjectCategoryList5(val) {
             }
         }
         html += "</select>";
-        //innerHTML(tagId, html);
-        //$("#prod_list").find("tbody").find("tr").eq(index).find("td").eq(0).html(html);
         $(".mobiletestContent1").find("span").eq(0).html(html);
     });
 }
@@ -675,8 +669,6 @@ function getTeacherSubjectCategoryList10(val) {
             }
         }
         html += "</select>";
-        //innerHTML(tagId, html);
-        //$("#prod_list").find("tbody").find("tr").eq(index).find("td").eq(0).html(html);
         $(".mobiletestContent2").find("span").eq(0).html(html);
     });
 }
@@ -731,32 +723,9 @@ function getTeacherSubjectCategoryList7(val) {
             }
         }
         html += "</select>";
-        //innerHTML(tagId, html);
-        //$("#prod_list").find("tbody").find("tr").eq(index).find("td").eq(0).html(html);
         $(".mobiletestContent").find("span").eq(0).html(html);
     });
 }
-
-
-//강사관리 - 과목 셀렉트박스 123
-function getMemberTeacerCategoryList(val) {
-    selectboxService.getCategoryList(3710, function (list) {
-        var html = "<select name='subjectCtgKey' class='col-sm-3 select2 form-control custom-select'>";
-        html += "<option value='' selected>선택</option>";
-        for (var i=0; i<list.length; i++) {
-            if(list[i].ctgKey == val){
-                html += "<option value="+list[i].ctgKey+" selected>"+ list[i].name +"</option>";
-            }else{
-                html += "<option value="+list[i].ctgKey+">"+ list[i].name +"</option>";
-            }
-        }
-        html += "</select>";
-        innerHTML(tagId, html);
-    });
-}
-
-
-
 
 function getTeacherSubjectCategoryList1(tagId, val) {
     selectboxService.getCategoryList(3710, function (list) {
@@ -773,7 +742,6 @@ function getTeacherSubjectCategoryList1(tagId, val) {
         innerHTML(tagId, html);
     });
 }
-
 
 function defaultCategorySelectbox() {
     var html = "<select id='sel_category' name='selCategory[]' onchange='' class='form-control'>";
@@ -1255,11 +1223,12 @@ function getEmailSelectbox(tagId, val) {
 function getlectureWatchPayStatusSelectbox(tagId, val) {
     var html = "<select id='PayStatus' class='col-sm-5 select2 form-control custom-select'>";
     var selected = '';
+    var selected1 = '';
     if(val == '2') selected = 'selected';
-    else if(val == '8') selected = 'selected';
+    else if(val == '8') selected1 = 'selected';
 
     html += "<option value='2' "+ selected +">결제완료</option>";
-    html += "<option value='8' "+ selected +">결제취소</option>";
+    html += "<option value='8' "+ selected1 +">결제취소</option>";
     html += "</select>";
 
     innerHTML(tagId, html);
@@ -1358,34 +1327,45 @@ function getAcaLecturePayTypeSelectbox(tagId, val) {
 function getCardKindSelectbox(tagId, val) {
     var html = "<select id='cardCode' class='col-sm-3 select2 form-control custom-select'>";
     var selected = '';
+    var selected1 = '';
+    var selected2 = '';
+    var selected3 = '';
+    var selected4 = '';
+    var selected5 = '';
+    var selected6 = '';
+    var selected7 = '';
+    var selected8 = '';
+    var selected9 = '';
+    var selected10 = '';
+    var selected11 = '';
+    var selected12 = '';
     if(val == '0') selected = 'selected';
-    else if(val == '01') selected = 'selected';
-    else if(val == '03') selected = 'selected';
-    else if(val == '06') selected = 'selected';
-    else if(val == '04') selected = 'selected';
-    else if(val == '12') selected = 'selected';
-    else if(val == '11') selected = 'selected';
-    else if(val == '14') selected = 'selected';
-    else if(val == '16') selected = 'selected';
-    else if(val == '17') selected = 'selected';
-    else if(val == '900') selected = 'selected';
-    else if(val == '901') selected = 'selected';
-    else selected = 'selected';
+    else if(val == '01') selected1 = 'selected';
+    else if(val == '03') selected2 = 'selected';
+    else if(val == '06') selected3 = 'selected';
+    else if(val == '04') selected4 = 'selected';
+    else if(val == '12') selected5 = 'selected';
+    else if(val == '11') selected6 = 'selected';
+    else if(val == '14') selected7 = 'selected';
+    else if(val == '16') selected8 = 'selected';
+    else if(val == '17') selected9 = 'selected';
+    else if(val == '900') selected10 = 'selected';
+    else if(val == '901') selected11 = 'selected';
+    else selected12 = 'selected';
 
     html += "<option value='0' "+ selected +">카드선택</option>";
-    html += "<option value='01' "+ selected +">외환</option>";
-    html += "<option value='03' "+ selected +">롯데</option>";
-    html += "<option value='06' "+ selected +">국민</option>";
-    html += "<option value='04' "+ selected +">현대</option>";
-    html += "<option value='12' "+ selected +">삼성</option>";
-    html += "<option value='11' "+ selected +">BC</option>";
-    html += "<option value='14' "+ selected +">신한</option>";
-    html += "<option value='16' "+ selected +">NH</option>";
-    html += "<option value='17' "+ selected +">하나 SK</option>";
-    html += "<option value='900' "+ selected +">기업</option>";
-    html += "<option value='901' "+ selected +">우리</option>";
-    html += "<option value='999' "+ selected +">기타</option>";
-
+    html += "<option value='01' "+ selected1 +">외환</option>";
+    html += "<option value='03' "+ selected2 +">롯데</option>";
+    html += "<option value='06' "+ selected3 +">국민</option>";
+    html += "<option value='04' "+ selected4 +">현대</option>";
+    html += "<option value='12' "+ selected5 +">삼성</option>";
+    html += "<option value='11' "+ selected6 +">BC</option>";
+    html += "<option value='14' "+ selected7 +">신한</option>";
+    html += "<option value='16' "+ selected8 +">NH</option>";
+    html += "<option value='17' "+ selected9 +">하나 SK</option>";
+    html += "<option value='900' "+ selected10 +">기업</option>";
+    html += "<option value='901' "+ selected11 +">우리</option>";
+    html += "<option value='999' "+ selected12 +">기타</option>";
     html += "</select>";
     innerHTML(tagId, html);
 }
@@ -1393,10 +1373,11 @@ function getCardKindSelectbox(tagId, val) {
 function deviceManageSelectbox(tagId, val) {
     var html = "<select id='deivceSel' class='form-control' onchange='changeDeviceList(this.value);'>";
     var selected = '';
+    var selected1 = '';
     if(val == '0') selected = 'selected';
-    else if(val == '1') selected = 'selected';
+    else if(val == '1') selected1 = 'selected';
     html += "<option value='0' "+ selected +">상품별 디바이스 관리</option>";
-    html += "<option value='1' "+ selected +">모바일 디바이스 관리</option>";
+    html += "<option value='1' "+ selected1 +">모바일 디바이스 관리</option>";
     html += "</select>";
     innerHTML(tagId, html);
 }
@@ -1549,12 +1530,12 @@ function searchCounselSelectBox(tagId) {
 function getAuthoritySelectbox(tagId, val) {
     var html = "<select id='authoritSel' class='form-control'>";
     var selected = "";
-
+    var selected2 = "";
     if(val == '0') selected = 'selected';
-    else if(val == '5') selected = 'selected';
+    else if(val == '5') selected2 = 'selected';
 
     html += "<option value=0 "+ selected +">관리자</option>";
-    html += "<option value=5 "+ selected +">강사</option>";
+    html += "<option value=5 "+ selected2 +">강사</option>";
     html += "</select>";
     innerHTML(tagId, html);
 }
@@ -1563,11 +1544,11 @@ function getAuthoritySelectbox(tagId, val) {
 function getAuthorityGradeSelectbox(tagId, val) {
     var html = "<select id='authoritGradeSel' class='form-control'>";
     var selected = "";
-
+    var selected1 = "";
     if(val == '0') selected = 'selected';
-    else if(val == '5') selected = 'selected';
+    else if(val == '5') selected1 = 'selected';
     html += "<option value=0 "+ selected +">관리자</option>";
-    html += "<option value=5 "+ selected +">강사</option>";
+    html += "<option value=5 "+ selected1 +">강사</option>";
     html += "</select>";
     innerHTML(tagId, html);
 }
