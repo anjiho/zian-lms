28a9ca272537aaf558fed601454aa601ca5f4e5c
anjiho <anjo0080@gmail.com>
2020-03-31 15:31:53 +0900

    ./

--- src/main/java/com/zianedu/lms/service/ScheduleService.java
+++ src/main/java/com/zianedu/lms/service/ScheduleService.java
@@ -35,43 +35,4 @@ public class ScheduleService {
         String yyyymmdd = Util.plusDate(Util.returnNow(), -1);
         teacherCalculateRepository.calculateTeacherSaleGoodsAny(yyyymmdd);
     }
-
-    @DataSource(DataSourceType.GW_ORACLE)
-    public void daySchedule() throws Exception {
-        List<HashMap<String, Object>>list = testMapper.selectMyTable();
-        this.insertBrdWork(list);
-
-    }
-
-    @DataSource(DataSourceType.GW_ORACLE)
-    public void insertBrdWork(List<HashMap<String, Object>>list) {
-        for (HashMap<String, Object> listMap : list) {
-            String userId = String.valueOf(listMap.get("USER_ID"));
-
-            String userName = "";
-            String title = "";
-            String email = "";
-            if (userId.equals("10062")) {
-                userName = "안지호";
-                title = "업무일지_안지호";
-                email = "anjo0070@zianedu.com";
-            } else if (userId.equals("10063")) {
-                userName = "원은정";
-                title = "업무일지_원은정";
-                email = "ejwon@zianedu.com";
-            }
-
-            int idx = testMapper.selectWorkCurrentIdx();
-            HashMap<String, Object> paramMap = new HashMap<>();
-            paramMap.put("idx", idx + 1);
-            paramMap.put("userId", listMap.get("USER_ID"));
-            paramMap.put("name", userName);
-            paramMap.put("email", email);
-            paramMap.put("title", title);
-            paramMap.put("contents", listMap.get("CONTENT"));
-
-            testMapper.insertBrdWork(paramMap);
-        }
-    }
-
 }
